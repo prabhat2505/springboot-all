de6b1b810cf53ef5caa8a196507d44130d4c21ff
Prabhat Rana <prabhat.rana828@gmail.com>
2023-11-17 23:24:53 +0530

    first commit

---
+++ FileProcessor/src/test/java/dataprocessor/FileProcessor/FileProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package dataprocessor.FileProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
